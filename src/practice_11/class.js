7c57786587359f10834a1d1b040af990a741b697
summer00072 <1573819207@qq.com>
2018-05-04 21:38:56 +0800

    all successful

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -5,15 +5,28 @@ class Class {
 	getDisplayName() {
 		return `Class ${this.number}`;
 	};
+	registerAssignLeaderListener(teacher) {
+		this.teacherOne = teacher;	
+	};
+	registerJoinListener(teacher) {
+		this.teacherTwo = teacher;
+    };
 	assignLeader(student) {
 		if (student.class.number == this.number) {
 			this.leader = student;
-		} else {
+			if (this.teacherOne && this.teacherOne.classes.indexOf(this.leader.class) != -1 ) {
+				console.log(`I am Tom. I know ${student.name} become Leader of Class ${student.class.number}.`);
+			}
+		}
+		 else {
 			console.log(`It is not one of us.`);
 		}
 	};
 	appendMember(student) {
 		student.class = this;
+		if (this.teacherTwo && this.teacherTwo.classes.indexOf(student.class) != -1 ) {
+			console.log(`I am Tom. I know ${student.name} has joined Class ${student.class.number}.`);
+		}				
 	};
 };
 module.exports = Class;
